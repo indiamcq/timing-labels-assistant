dd6fc33470d3d7b12580fa2e7881053c069f2628
indiamcq <india.mcq@gmail.com>
2015-09-09 14:48:38 +1000

    compile script and branch changes

---
+++ compile-current-version.cmd
@@ -0,0 +1,5 @@
+@echo off
+set compiler=C:\programs\autohotkey32\Ahk2Exe112202\ahk2exe.exe
+set bin=C:\programs\autohotkey32\Ahk2Exe112202\Unicode 32-bit.bin
+if not exist "%cd%\current" md "%cd%\current"
+call "%compiler%" /in Timing-Labels-Assistant.ahk /out current\Timing-Labels-Assistant.exe /icon Timing-Labels-Assistant.ico /bin "%bin%"
