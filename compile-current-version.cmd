61445f0a9bd89b92941ab9a2a95aa5169e98659a
indiamcq <india.mcq@gmail.com>
2015-09-17 05:53:35 +1000

    added jade transformation for README.md

--- compile-current-version.cmd
+++ compile-current-version.cmd
@@ -3,3 +3,5 @@ set compiler=C:\programs\autohotkey32\Ahk2Exe112202\ahk2exe.exe
 set bin=C:\programs\autohotkey32\Ahk2Exe112202\Unicode 32-bit.bin
 if not exist "%cd%\current" md "%cd%\current"
 call "%compiler%" /in Timing-Labels-Assistant.ahk /out current\Timing-Labels-Assistant.exe /icon Timing-Labels-Assistant.ico /bin "%bin%"
+jade -o current/ jade/Timing-Labels-Assistant_readme.jade
+pause
