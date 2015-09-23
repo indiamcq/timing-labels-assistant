6177aadf37209523fef78b23b8f80c4d881aa37f
indiamcq <india.mcq@gmail.com>
2015-09-23 10:52:08 +0800

    revised README.md and compile-current-version.cmd

--- compile-current-version.cmd
+++ compile-current-version.cmd
@@ -4,5 +4,7 @@ set bin=C:\programs\autohotkey32\Ahk2Exe112202\Unicode 32-bit.bin
 if not exist "%cd%\current" md "%cd%\current"
 call "%compiler%" /in Timing-Labels-Assistant.ahk /out current\Timing-Labels-Assistant.exe /icon Timing-Labels-Assistant.ico /bin "%bin%"
 call jade -o current/ jade/Timing-Labels-Assistant_readme.jade
-call "C:\Program Files\7-Zip\7z.exe" a  -tzip current\Timing-Labels-Assistant_v1.2.27.zip current\*.exe current\*.html
+cd current
+call "C:\Program Files\7-Zip\7z.exe" a  -tzip Timing-Labels-Assistant_v1.2.27.zip *.exe *.html
+cd ..
 pause
